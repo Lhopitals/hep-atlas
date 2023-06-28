b400555aea86d3d7dde6e42aaf3eff1092dab492
tomilee_ubuntu <joriv.yanez@ug.uchile.cl>
2023-06-27 22:30:00 -0400

    calo_param_fix mejorado

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     elif input==2:
         path = '/home/tomilee/Desktop/Universidad/dark_photons/analisis_datos_github/hep-atlas/data/'
-        data_yaml = read_data_yaml('calo_param.yaml')      #Parámetros del calo
+        data_yaml = read_data_yaml('calo_param_fix.yaml')      #Parámetros del calo
 
     else:
         print("ingrese un número válido")
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     #data_yaml = read_data_yaml('previous_data_muon.yaml')  #De prueba para muonic
     #data_yaml = read_data_yaml('previous_data_calo.yaml')  #De prueba para muonic
 
-    data_yaml = read_data_yaml('muonic_param.yaml')    #Parámetros del muonic
-    #data_yaml = read_data_yaml('calo_param.yaml')      #Parámetros del calo
+    # data_yaml = read_data_yaml('muonic_param.yaml')    #Parámetros del muonic
+    # data_yaml = read_data_yaml('calo_param.yaml')      #Parámetros del calo
 
     # de data_yaml obtenemos los nombres de los datasets
     signals = data_yaml['signals'].values() #esto devuelve solo los valores de cada variable.
